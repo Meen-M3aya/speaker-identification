ea868dd723d050edf106b76f863756c381b3fcf9
Cellzawy <ahmed.amr.elhossieny@gmail.com>
2025-05-20 16:47:32 +0300

    fixed a misleading console log

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ namespace Recorder
         [STAThread]
         static void Main()
         {
-            //TestingDTW.sampling();
+            TestingDTW.sampling(0);
+            TestingDTW.sampling(11);
+            TestingDTW.sampling(63);
             //TestingDTW.TestCase(1, 23);
-            TestingDTW.TestCase(1, 0);
+            //TestingDTW.TestCase(1, 0);
             //TestingDTW.TestCase(2, 55);
             //TestingDTW.TestCase(3, 11);
 
